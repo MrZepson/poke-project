e82d3c2311267d643f054174286b475d4e5c05a7
Nelly Negm <nene@Nellys-MacBook-Air.local>
2022-03-11 12:13:45 +0100

    poke-card progress-desu

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,8 +2,12 @@ const Pokemon = ({id,name}) => {
     console.log(id)
     return (
         
-        <div>
-            {name}
+        <div className="poke-card-container">
+            <div className="poke-img-container"><img className="poke-img" src="#" alt={name} /></div>
+            <div className="poke-name-container"> 
+            <h2>{name}</h2>
+            <h1>#{id}</h1>
+            </div>
         </div>
     )
 }
