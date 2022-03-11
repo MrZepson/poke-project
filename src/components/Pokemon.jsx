8b693e2eb3b2d3b84d9b7d0b226247818b7db035
Nelly Negm <nene@Nellys-MacBook-Air.local>
2022-03-11 13:58:47 +0100

    ard css progress

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,12 +1,16 @@
+import "../styles/pokemon.css"
+
 const Pokemon = ({id,name}) => {
     console.log(id)
     return (
         
         <div className="poke-card-container">
-            <div className="poke-img-container"><img className="poke-img" src="#" alt={name} /></div>
+            <div className="poke-img-container"> 
+                <img className="poke-img" src="#" alt={name} />
+            </div>
             <div className="poke-name-container"> 
-            <h2>{name}</h2>
-            <h1>#{id}</h1>
+                <h2>{name}</h2>
+                <h1>#{id}</h1>
             </div>
         </div>
     )
