8353670c09d9f005b352cdaae81b7df393946ef3
Christian Markus <cm50@msn.com>
2022-04-06 19:45:13 +0200

    sista search försöket

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,8 @@
-// import styles from "./SearchBar.module.css";
-// import { Pokedex } from "./Pokedex"
+import styles from "./SearchBar.module.css";
+import { Pokedex } from "./Pokedex"
 
 const SearchBar = () => {
+  /*
   //tar emot från local storage
   let pokemons = localStorage.getItem('pokemons');
   console.log(pokemons);
@@ -27,12 +28,13 @@ const SearchBar = () => {
     //sätter en ny local storage med bara matchningarna
     localStorage.setItem('matches', JSON.stringify(matches));
   }
-
+*/
   return (
     <div className="">
         <div className="Card">
-          <input type="text" onChange={(e)=>search(e.target.value)} ></input>
-          {/* <Pokedex /> */}
+        
+        {/*}  <input type="text" onChange={(e)=>search(e.target.value)} ></input> */}
+          <Pokedex />
           {/*<input className={styles.searchbar} type="text" placeholder="Search Pokemon..." />*/}
         </div>
     </div>
